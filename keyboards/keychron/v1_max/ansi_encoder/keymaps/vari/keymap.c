af81692d44a65e91ac4396b8e50661b9a236d2ba
Vari <72755263+variapolis@users.noreply.github.com>
2024-11-10 21:28:20 +0000

    fix: disable debugging in keymap

--- keyboards/keychron/v1_max/ansi_encoder/keymaps/vari/keymap.c
+++ keyboards/keychron/v1_max/ansi_encoder/keymaps/vari/keymap.c
@@ -74,7 +74,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
-
+/*
 void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
   debug_enable=true;
@@ -82,3 +82,4 @@ void keyboard_post_init_user(void) {
   debug_keyboard=true;
   //debug_mouse=true;
 }
+*/
